3c07a0b0ad9454c37a59fc86de42e394c69e54ab
Arthur Police <arthurpolice1@gmail.com>
2023-12-23 15:20:22 +0100

    fixing margins

--- src/app/_components/contacts/index.tsx
+++ src/app/_components/contacts/index.tsx
@@ -6,15 +6,15 @@ export default function Contacts() {
   return (
     <span className='flex items-center justify-evenly w-full mt-8 py-20'>
       <span className='flex flex-col items-center justify-center text-slate-700 text-m font-josefinSans font-bold'>
-        <FaWhatsapp className='size-24' />
+        <FaWhatsapp className='size-16 xl:size-24' />
         Whatsapp
       </span>
       <span className='flex flex-col items-center justify-center text-slate-700 text-m font-josefinSans font-bold'>
-        <SiGmail className='size-24' />
+        <SiGmail className='size-16 xl:size-24' />
         E-Mail
       </span>
       <span className='flex flex-col items-center justify-center text-slate-700 text-m font-josefinSans font-bold'>
-        <FaInstagram className='size-24' />
+        <FaInstagram className='size-16 xl:size-24' />
         Instagram
       </span>
     </span>
--- src/app/_views/home/components/card.tsx
+++ src/app/_views/home/components/card.tsx
@@ -4,7 +4,7 @@ interface CardProps {
 
 export default function Card({ children }: CardProps) {
   return (
-    <div className='w-full min-h-[500px] bg-white/60 rounded-lg flex justify-center items-center flex-col p-4 xl:p-12'>
+    <div className='w-full min-h-[500px] bg-white/60 rounded-lg flex justify-center items-center flex-col my-6 p-4 xl:p-12'>
       {children}
     </div>
   )
